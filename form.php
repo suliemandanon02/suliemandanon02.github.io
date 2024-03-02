fa8191d9cb9476e829a33111f67773b2c364cb93
suliemandanon02 <suliemandanon02@gmail.com>
2024-03-02 12:30:21 -0500

    workin on php

---
+++ form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Submission</title>
+</head>
+<body>
+  <h2>Submit Form Data</h2>
+  <form action="/xxx.php" method="get" name="myForm">
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname"><br><br>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname"><br><br>
+    <input type="submit" value="Send form data!">
+  </form>
+
+<?php
+if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "GET") {
+    // Retrieve form data
+    $fname = isset($_GET['fname']) ? $_GET['fname'] : '';
+    $lname = isset($_GET['lname']) ? $_GET['lname'] : '';
+
+    // Do something with the data
+    echo "First Name: " . $fname . "<br>";
+    echo "Last Name: " . $lname;
+}
+?>
+</body>
+</html>
