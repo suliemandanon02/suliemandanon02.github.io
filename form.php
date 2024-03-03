e9ec176c8074dcdbb0f968bf48ebca2734c55fd5
suliemandanon02 <suliemandanon02@gmail.com>
2024-03-03 14:29:11 -0500

    added lenth name

--- form.php
+++ form.php
@@ -39,6 +39,26 @@
     }
     ?>
   </div>
+  <?php
+
+// Check if the username is provided in the URL parameters
+if(isset($_GET['username'])) {
+    // Retrieve the username from the URL parameters
+    $name = $_GET['username'];
+
+    // Count the number of characters in the username
+    $nameLength = strlen($name);
+
+    // Output the concatenated string
+    $fullGreeting = "Hello " . $name . "!";
+    echo $fullGreeting . "<br>";
+
+    // Output the length of the username
+    echo "The length of your username is: " . $nameLength;
+} else {
+    echo "Username not provided in the URL parameters.";
+}
+?>
 
   <!-- Go Back Button -->
   <button class="goback-button" onclick="goBack()">Go Back</button>
