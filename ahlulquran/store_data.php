69cd1738526dbbe8c0a774410be0015466697a4d
suliemandanon02 <suliemandanon02@gmail.com>
2024-04-14 16:02:12 -0400

    updated pass

--- ahlulquran/store_data.php
+++ ahlulquran/store_data.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "your_mysql_password"; // Replace with your MySQL root password
+$password = "rootpass"; // Replace with your MySQL root password
 $dbname = "ahlulquran";
 
 // Create connection
